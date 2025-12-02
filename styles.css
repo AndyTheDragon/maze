768f62d1c9cd8bb72d1556194219ad9235693a67
AndyTheDragon <andre.jansam@gmail.com>
2025-12-02 09:08:33 +0100

    first commit

---
+++ styles.css
@@ -0,0 +1,59 @@
+body {
+    background-color: #666;
+    color: #6b6;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+#info {
+    width: fit-content;
+    margin: 0 auto;
+    color: #ddd;
+    font-size: 1.1rem;
+    text-align: center;
+}
+
+#grid {
+    display: grid;
+    grid-template-columns: repeat(10, max-content);
+    border-left: 1px dashed darkgrey;
+    border-top: 1px dashed darkgrey;
+    width: fit-content;
+    margin: 20% auto;
+    margin-bottom: 20px;
+}
+
+.cell {
+    width: 64px;
+    aspect-ratio: 1 / 1;
+    background-color: #777;
+    border-right: 1px dashed darkgrey;
+    border-bottom: 1px dashed darkgrey;
+}
+
+.player {
+    background-color: #6b6;
+}
+
+.goal {
+    background-color: #cc6;
+}
+
+#pauseOverlay {
+    display: none;
+    position: fixed;
+    inset: 0; /* top:0; right:0; bottom:0; left:0; */
+    background: rgba(0,0,0,0.5);
+    color: #fff;
+    font-weight: 700;
+    font-size: 2.25rem;
+    align-items: center;
+    justify-content: center;
+    z-index: 999;
+    pointer-events: none; /* allow keyboard input to still reach document */
+    text-transform: uppercase;
+    letter-spacing: 0.08em;
+}
+
+#pauseOverlay.visible {
+    display: flex;
+}
